7e90c2a6625c21c1e31e0bfe13cbaf254c33a368
oussayd <oussayd@users.noreply.github.com>
2016-11-04 17:27:56 +0100

    fin data ous

--- selenium/data.js
+++ selenium/data.js
@@ -108,7 +108,90 @@ exports.data = [
 
         categorie: "cat6"
     },
+      {
+        titre: "Casque PlayStation 4 Gioteck HS-1 neuf",
+        desc: "Casque PlayStation 4 Gioteck HS-1 Superlite NEUF avec microphone.\n\n"+
+              "Rdv possible à la Défense, Les Halles ou garde de Lyon"      ,
+        prix: "12",
+        image0: "ps4\\Gioteck-01.jpeg",
+        categorie: "cat6"
+    },    
+        {
+        titre: "Jeu PS4 Battleborn 4 neuf sous blister",
+        desc: "Jeu Battleborn 4 PS4 neuf sous blister.\n\n"+
+              "Rdv possible à la Défense, Les Halles ou garde de Lyon"      ,
+        prix: "5",
+        image0: "ps4\\Battleborn-01.jpeg",
+        categorie: "cat6"
+    },    
+         {
+        titre: "Kit Mains libres Bluetooth Supertooth neuf",
+        desc: "Kit Mains libres Bluetooth Supertooth\n"+ 
+               "Etat : NEUF dans sa boite d'origine jamais utilisé.\n\n"+
+              "Rdv possible à la Défense, Les Halles ou garde de Lyon"      ,
+        prix: "19",
+        image0: "ht\\Supertooth-01.jpeg",
+        categorie: "cat6"
+    }
+        ,    
+ {
+        titre: "Antivol articulé ABUS CITADEL 120cmX25mm neuf",
+        desc: "Antivol articulé ABUS CITADEL 120cmX25mm neuf avec 2 clefs\n"+ 
+               "Prix en magasin autour de 25Euro(s)\n\nPrix ferme.\n"+
+              "Rdv possible à la Défense, Les Halles ou garde de Lyon"      ,
+        prix: "15",
+        image0: "moto\\AntivolCITADEL-01.jpeg",
+        categorie: "cat6"
+    }
+        ,    
+         {
+        titre: "Barres de toit Neuves modus golf V 405",
+        desc: "Barres de toit Green Valley neuves jamais motées "+
+        +"\nCompatibes avec les modèles suivants : "
+        +"\nRenault Modus "
+        +"\nRenault Grand Modus "
+        +"\nPeugeot 405 "
+        +"\nCitroen Xantia"
+        +"\nLancia Kappa"
+        +"\nVolkswagen Golf V"      ,
+        prix: "55",
+        image0: "barres\\gvGolfV-01.jpeg",
+        categorie: "cat6"
+    }
+        ,    
+ {
+        titre: "Sac bandoulière QUIKSILVER neuf",
+        desc: "Sac bandoulière QUIKSILVER Tocador Patch neuf jamais utilisé"+ 
+        +"\nSac à bandoulière Quiksilver en polyester 600d." 
+        +"\n1 compartiment principal."
+        +"\nFermeture par rabat avec bande velcro. "
+        +"\nCompartiment ordinateur. "
+        +"\nBandoulière réglable. "
+        +"\nPoche zippée."
+        +"\n"
+        +"\nPrix en magasin autour de 50Euro(s)"
+        +"\nRdv possible à la Défense, Les Halles ou garde de Lyon"      ,
+        prix: "23",
+        image0: "acc\\sacQUIKSILVER-01.jpeg",
+        categorie: "cat6"
+    }
+              ,    
+ {
+        titre: "Barres de toit Summit pour Citroën C1",
+        desc: "Barres de toit Summit pour Citroën C1 entre 2005 et 2013"
+        +"\nEtat NEUF"      ,
+        prix: "55",
+        image0: "barres\\citroenC1-01.jpeg",
+        categorie: "cat6"
+    }   ,    
+ {
+        titre: "Barres de toit Peugeot 206 3p comme neuves",
+        desc: "Barres de toit Peugeot 206 3p d'origine marque Peugeot avec serrure et deux clefs antivol fournies"
+        +"\nEtat comme neuf"      ,
+        prix: "65",
+        image0: "barres\\peugeot206-01.jpeg",
+        categorie: "cat6"
+    }
         
-       
 ]
 
